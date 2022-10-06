bbd032a34dab3cd2da3844bbf2d63a917e787950
Rolson Quadras <rolson.quadras@avast.com>
2022-10-06 15:07:47 -0400

    chore: Add lint and unit-test
    Signed-off-by: Rolson Quadras <rolson.quadras@avast.com>

---
+++ scripts/check_unit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Copyright SecureKey Technologies Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+set -e
+
+echo "Running $0"
+
+pwd=`pwd`
+touch "$pwd"/coverage.out
+
+amend_coverage_file () {
+if [ -f profile.out ]; then
+    cat profile.out | grep -v ".gen.go" >> "$pwd"/coverage.out
+    rm profile.out
+fi
+}
+
+# Running wallet-sdk unit tests
+PKGS=`go list github.com/trustbloc/wallet-sdk/... 2> /dev/null | \
+                                                  grep -v /mocks`
+go test $PKGS -count=1 -race -coverprofile=profile.out -covermode=atomic -timeout=10m
+amend_coverage_file
+
+cd "$pwd"
