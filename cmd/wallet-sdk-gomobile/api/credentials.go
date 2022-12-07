a1cc906e2e26d7aa163a92a9c251e06b86be0471
Volodymyr Kubiv <volodymyr.kubiv@euristiq.com>
2022-12-07 18:17:43 +0200

    feat: typed credential and credential array
    Signed-off-by: Volodymyr Kubiv <volodymyr.kubiv@euristiq.com>

---
+++ cmd/wallet-sdk-gomobile/api/credentials.go
@@ -0,0 +1,44 @@
+/*
+Copyright Avast Software. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package api
+
+// VerifiableCredential typed wrapper around verifiable credentials content.
+type VerifiableCredential struct {
+	Content []byte
+}
+
+// NewVerifiableCredential creates new VerifiableCredential.
+func NewVerifiableCredential(content []byte) *VerifiableCredential {
+	return &VerifiableCredential{
+		Content: content,
+	}
+}
+
+// VerifiableCredentialsArray ia a wrapper around go array of VerifiableCredential to overcome limitations of gomobile.
+type VerifiableCredentialsArray struct {
+	credentials []*VerifiableCredential
+}
+
+// NewVerifiableCredentialsArray creates new NewVerifiableCredentialsArray.
+func NewVerifiableCredentialsArray() *VerifiableCredentialsArray {
+	return &VerifiableCredentialsArray{}
+}
+
+// Add adds new VerifiableCredential to underlying array.
+func (a *VerifiableCredentialsArray) Add(cred *VerifiableCredential) {
+	a.credentials = append(a.credentials, cred)
+}
+
+// Length returns length of underlying array.
+func (a *VerifiableCredentialsArray) Length() int {
+	return len(a.credentials)
+}
+
+// AtIndex returns element from underlying array by index.
+func (a *VerifiableCredentialsArray) AtIndex(index int) *VerifiableCredential {
+	return a.credentials[index]
+}
--- cmd/wallet-sdk-gomobile/go.mod
+++ cmd/wallet-sdk-gomobile/go.mod
@@ -54,7 +54,7 @@ require (
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
-	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
