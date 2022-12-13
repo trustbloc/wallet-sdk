30a428f1720b1fa132ef9f398f1d7d588c1f7311
Talwinder kaur <talwinder.kaur@avast.com>
2022-12-13 03:23:25 -0500

    feat: Update credential list to show details in nice format
    Signed-off-by: Talwinder kaur <talwinder.kaur@avast.com>

--- demo/app/lib/widgets/credential_card.dart
+++ demo/app/lib/widgets/credential_card.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:app/models/store_credential_data.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:app/models/credential_preview.dart';
 
 class CredentialCard extends StatefulWidget {
   StorageItem item;
@@ -13,10 +16,47 @@ class CredentialCard extends StatefulWidget {
 
 class _CredentialCardState extends State<CredentialCard> {
   bool _visibility = false;
-  final _storage = const FlutterSecureStorage();
+
+  Widget getCredentialDetails() {
+    List<CredentialPreviewData> list;
+    var data = json.decode(widget.item.value);
+    var credentialClaimsData = data['credential_displays'][0]['claims'] as List;
+    list = credentialClaimsData.map<CredentialPreviewData>((json) => CredentialPreviewData.fromJson(json)).toList();
+    return listViewWidget(list);
+  }
+
+  Widget listViewWidget(List<CredentialPreviewData> credPrev) {
+    return ListView.builder(
+        itemCount: credPrev.length,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, position) {
+          //TODO Ignoring the photo value for now due to extremely long text need to render in a separate issue-881
+          return (credPrev[position].label != "photo") ? Card(
+            child: ListTile(
+              title: Text(
+                credPrev[position].label,
+                style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.normal),
+              ),
+              subtitle: Text(
+                credPrev[position].value,
+                style: const TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.green,
+                    fontWeight: FontWeight.normal),
+              ),
+            ),
+          ):
+          Container();
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Map<String, dynamic> issuer = jsonDecode(widget.item.value);
+    final credentialDisplayName = issuer['credential_displays'][0]['overview']['name'];
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4),
       child: Container(
@@ -36,21 +76,27 @@ class _CredentialCardState extends State<CredentialCard> {
               });
             },
             title: Text(
-              widget.item.key,
+              credentialDisplayName,
               style: const TextStyle(
                 fontSize: 18,
               ),
             ),
             subtitle: Visibility(
               visible: _visibility,
-              child: Text(
-                widget.item.value,
-                style: const TextStyle(fontWeight: FontWeight.bold),
+              child: SizedBox(
+                height: 500,
+                child: Column(
+                  children: [
+                    Flexible( // wrap in Expanded
+                      child: getCredentialDetails(),
+                    ),
+                  ],
+                ),
               ),
             ),
-            leading: const Icon(Icons.security, size: 30),
+            leading: const Icon(Icons.card_membership_outlined, size: 30),
             trailing: IconButton(
-              icon: const Icon(Icons.edit),
+              icon: const Icon(Icons.arrow_downward),
               onPressed: () async {
 
               },
