c47c66838b50eba3f72655c0471a62909eb56a34
Talwinder kaur <talwinder.kaur@avast.com>
2023-04-02 23:36:47 -0400

    fix(app): Add a qr code simulator if the device is not real
    Signed-off-by: Talwinder kaur <talwinder.kaur@avast.com>

--- demo/app/lib/views/scanner.dart
+++ demo/app/lib/views/scanner.dart
@@ -3,6 +3,9 @@ import 'package:app/scenarios/handle_openid_url.dart';
 import 'package:app/widgets/common_title_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:safe_device/safe_device.dart';
+
+import 'package:app/widgets/primary_button.dart';
 
 class QRScanner extends StatefulWidget {
   const QRScanner({Key? key}) : super(key: key);
@@ -16,10 +19,25 @@ class QRScanner extends StatefulWidget {
 class QRScannerState extends State<QRScanner> {
   QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  final textController = TextEditingController();
+  bool? isRealDevice;
+
+  @override
+  void initState(){
+    super.initState();
+    isDeviceReal();
+  }
+
+  isDeviceReal ()async{
+    bool isRealDeviceResp = await SafeDevice.isRealDevice;
+    setState((){
+      isRealDevice = isRealDeviceResp;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return isRealDevice! ? Scaffold(
       appBar: const CustomTitleAppBar(
         pageTitle: 'Scan QR',
         addCloseIcon: true,
@@ -37,6 +55,48 @@ class QRScannerState extends State<QRScanner> {
         ),
         onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
       ),
+    ):  Scaffold(
+      appBar: const CustomTitleAppBar(
+        pageTitle: 'QR Code Simulator',
+        addCloseIcon: true,
+        height: 50,
+      ),
+    body: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 32),
+          child: GestureDetector(
+            onDoubleTap:() {
+              if(textController.text.isNotEmpty) {
+                textController.selection = TextSelection(baseOffset: 0, extentOffset:textController.text.length);
+              }
+            },
+         child: TextField(
+            controller: textController,
+            keyboardType: TextInputType.multiline,
+            minLines: 1,
+            maxLines: 7,
+            autofocus: true,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Paste the qr code url',
+            ),
+          ),
+          )),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+        ),
+        PrimaryButton(
+            onPressed: () async {
+              log("whats the text ${textController.text.toString()}");
+              handleOpenIDUrl(context, textController.text.toString());
+            },
+            width: double.infinity,
+            child: const Text('Submit', style: TextStyle(fontSize: 16, color: Colors.white))
+        ),
+      ],
+     )
     );
   }
 
