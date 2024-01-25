3b0eb7f62fdbd9dc73ada94355805d1effd33639
Rolson Quadras <rolson.quadras@gendigital.com>
2024-01-25 14:18:01 -0500

    ci(sdk): update runner to macos-13
    Signed-off-by: Rolson Quadras <rolson.quadras@gendigital.com>

--- scripts/prepare_integration_test_flutter.sh
+++ scripts/prepare_integration_test_flutter.sh
@@ -68,7 +68,7 @@ healthCheck() {
 }
 
 # healthcheck
-healthCheck did-resolver http://did-resolver.trustbloc.local:8072/healthcheck 200
+healthCheck did-resolver http://did-resolver.trustbloc.local:8072/healthcheck 200 180
 healthCheck vc-rest http://localhost:8075/version 200 180
 
 if [ $healthCheckFailed == 1 ]
