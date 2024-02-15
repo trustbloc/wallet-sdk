e444c85e974dc41b998688d7c0bcd268e783d687
Volodymyr Kubiv <volodymyr.kubiv@euristiq.com>
2024-02-15 12:41:35 +0200

    feat(app): remove support of js wallet-sdk.
    Signed-off-by: Volodymyr Kubiv <volodymyr.kubiv@euristiq.com>

--- demo/app/lib/wallet_sdk/wallet_sdk.dart
+++ demo/app/lib/wallet_sdk/wallet_sdk.dart
@@ -4,6 +4,4 @@ Copyright Gen Digital Inc. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 
-export 'wallet_sdk_mobile.dart' // By default
-    if (dart.library.js) 'wallet_sdk_js.dart'
-    if (dart.library.io) 'wallet_sdk_mobile.dart';
+export 'wallet_sdk_mobile.dart';
