04361daf8290da4a642f2c9f5ecca90b5644677b
Anka <runner@Mac-1722533451390.local>
2024-08-01 17:58:40 +0000

    New walletsdk version 2.1.1-swift-pm

--- Package.swift
+++ Package.swift
@@ -9,9 +9,9 @@ SPDX-License-Identifier: Apache-2.0
 
 import PackageDescription
 
-let version = "2.1.0-swift-pm"
+let version = "2.1.1-swift-pm"
 let moduleName = "walletsdk"
-let checksum = "276ca63d5500843e4c36b202e488c47713e80ac7121f43ea5d6b37a96ccddefe"
+let checksum = "f5ba3e5b2c3d4d9279773da2c066ee0794215a7a732ae4062a99ef8121cdddef"
 
 let package = Package(
     name: moduleName,
